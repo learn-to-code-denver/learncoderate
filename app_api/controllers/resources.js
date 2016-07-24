794662c2426fa18f952462a3cf11c4402c12a243
jenyacazacu <evghenii0101@gmail.com>
2016-07-24 12:26:38 -0600

    api changes

--- app_api/controllers/resources.js
+++ app_api/controllers/resources.js
@@ -14,13 +14,28 @@ module.exports.resourcesList = function (req, res) {
 	sendJsonResponse(res, 200, {"status" : "success"});
 };
 module.exports.resourcesReadOne = function(req, res) { 
+	if(req.params && req.params.resourceid){
 	Res
 	   .findById(req.params.resourceid)
 	   .exec(function(err, resource) {
+	   	if(!resource){
+	   		sendJsonResponse(res, 404, {
+	   			"message":"Resource not found"
+	   		});
+	   		return;
+	   	} else if(err){
+	   		sendJsonResponse(res, 404,err);
+	   		return;
+	   	}
 	   	sendJsonResponse(res, 200, resource);
 	   });
+	} else {
+		sendJsonResponse(res, 404, {
+			"message":"No resource id in the request"
+		});
+	}
+	};
 
-};
 module.exports.resourcesUpdateOne = function(req, res) { };
 module.exports.resourcesDeleteOne = function(req, res) { };
 
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 # learncoderate_loc
+# connecting to the mongo instance 'mongo 127.0.0.1:27017/learncoderate'
