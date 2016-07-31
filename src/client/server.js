586ef9f37421ab5194e85b666fb3a4168fdef7dd
jenyacazacu <evghenii0101@gmail.com>
2016-07-31 14:09:24 -0600

    setup changes

---
+++ src/client/server.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+require('./app_api/models/db');
+
+var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index');
+var users = require('./app_server/routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server','views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir: __dirname + '/app_server/views/layouts' }))
+app.set('view engine', 'handlebars');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/api', routesApi);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
